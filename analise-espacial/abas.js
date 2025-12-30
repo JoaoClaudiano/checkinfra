dffa6d1be57582f0260c6ccc284b1b775b3c61ee
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2025-12-29 23:02:05 -0300

    Criar abas.js

---
+++ analise-espacial/abas.js
@@ -0,0 +1,6 @@
+function abrirAba(id) {
+  document.querySelectorAll(".secao").forEach(s => s.classList.remove("ativa"));
+  document.querySelectorAll(".abas button").forEach(b => b.classList.remove("ativa"));
+  document.getElementById(id).classList.add("ativa");
+  event.target.classList.add("ativa");
+}

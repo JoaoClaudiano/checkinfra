5c572d6968c847eaca1764387b5cdf6920b6ac1f
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2025-12-30 00:50:08 -0300

    Atualizar o pareto.js

--- analise-espacial/indicadores/pareto/pareto.js
+++ analise-espacial/indicadores/pareto/pareto.js
@@ -1,16 +1,16 @@
-const headersPareto = document.querySelectorAll(".accordion-header");
-
-headersPareto.forEach(header => {
+// pareto.js
+document.querySelectorAll(".accordion-header").forEach(header => {
   header.addEventListener("click", () => {
     const content = header.nextElementSibling;
     const isOpen = header.classList.contains("active");
 
-    headersPareto.forEach(h => {
+    // Fecha os outros accordions
+    document.querySelectorAll(".accordion-header").forEach(h => {
       h.classList.remove("active");
       h.nextElementSibling.style.maxHeight = null;
     });
 
-
+    // Abre o atual se não estava aberto
     if (!isOpen) {
       header.classList.add("active");
       content.style.maxHeight = content.scrollHeight + "px";

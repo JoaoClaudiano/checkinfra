c0eff8359692b6521998eebc484db076ce578181
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2025-12-29 23:16:44 -0300

    Criar pareto.js

---
+++ analise-espacial/indicadores/pareto/pareto.js
@@ -0,0 +1,18 @@
+const headersPareto = document.querySelectorAll(".accordion-header");
+
+headersPareto.forEach(header => {
+  header.addEventListener("click", () => {
+    const content = header.nextElementSibling;
+    const isOpen = header.classList.contains("active");
+
+    headersPareto.forEach(h => {
+      h.classList.remove("active");
+      h.nextElementSibling.style.maxHeight = null;
+    });
+
+    if (!isOpen) {
+      header.classList.add("active");
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
